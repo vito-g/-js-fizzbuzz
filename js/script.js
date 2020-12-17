0468e945136c03a8732a495817d5e1c151cb8f42
Vito Gentile <gentilevito1076@gmail.com>
2020-12-17 17:16:31 +0100

    ESERCIZIO COMPLETATO: Avevo dimenticato l'ultima condizione

--- js/script.js
+++ js/script.js
@@ -1,22 +1,25 @@
-// for ( var x=1; x <= 100; x++) {
-//
-//   console.log(x);
-//   if ((x % 3 == 0) && (x % 5 != 0)) {
-//     console.log('Fizz');
-//   }
-//   if ((x % 3 != 0) && (x % 5 == 0)) {
-//     console.log('Buzz');
-//   }
-//   else if ((x % 3 == 0) && (x % 5 == 0)) {
-//     console.log('FizzBuzz');
-//   }
-// }
+for ( var x=1; x <= 100; x++) {
+
+  if ((x % 3 == 0) && (x % 5 != 0)) {
+    console.log('Fizz');
+  }
+  if ((x % 3 != 0) && (x % 5 == 0)) {
+    console.log('Buzz');
+  }
+  else if ((x % 3 == 0) && (x % 5 == 0)) {
+    console.log('FizzBuzz');
+  }
+  else if ((x % 3 != 0) && (x % 5 != 0)){
+    console.log(x);
+  }
+
+}
 
 
 // MEGLIO così:
 
-for (var i = 1; i <= 100; i++) {
-  var x3 = i % 3 == 0;
-  var x5 = i % 5 == 0;
-  console.log(x3 ? x5 ? "FizzBuzz" : "Fizz" : x5 ? "Buzz" : i);
-}
+// for (var i = 1; i <= 100; i++) {
+//   var x3 = i % 3 == 0;
+//   var x5 = i % 5 == 0;
+//   console.log(x3 ? x5 ? "FizzBuzz" : "Fizz" : x5 ? "Buzz" : i);
+// }
