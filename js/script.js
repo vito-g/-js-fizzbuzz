07710589eab4061d5401519a3d063727a6d9a1cd
Vito Gentile <gentilevito1076@gmail.com>
2020-12-17 15:29:11 +0100

    Correzione

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ for ( var x=1; x <= 100; x++) {
   if (x % 5 == 0) {
     console.log('Buzz');
   }
-  else if ((x % 3 == 0) && (x % 3 == 0)) {
+  else if ((x % 3 == 0) && (x % 5 == 0)) {
     console.log('FizzBuzz');
   }
 }
