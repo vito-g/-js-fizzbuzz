9080568caa1dea2a03c7c1452b5f508d60b981bf
Vito Gentile <gentilevito1076@gmail.com>
2020-12-17 15:26:29 +0100

    INdividuati i multipli di 3 o di 5 e di entrambi

--- js/script.js
+++ js/script.js
@@ -2,7 +2,11 @@ for ( var x=1; x <= 100; x++) {
   console.log(x);
   if (x % 3 == 0)  {
     console.log('Fizz');
-  } else if (x % 5 == 0) {
+  }
+  if (x % 5 == 0) {
     console.log('Buzz');
-  } 
+  }
+  else if ((x % 3 == 0) && (x % 3 == 0)) {
+    console.log('FizzBuzz');
+  }
 }
