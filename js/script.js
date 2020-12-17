97e9a917d0b991dcb81502e170030145eb459799
Vito Gentile <gentilevito1076@gmail.com>
2020-12-17 15:32:20 +0100

    Correzione-02

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 for ( var x=1; x <= 100; x++) {
   console.log(x);
-  if (x % 3 == 0)  {
+  if ((x % 3 == 0) && (x % 5 != 0)) {
     console.log('Fizz');
   }
-  if (x % 5 == 0) {
+  if ((x % 3 != 0) && (x % 5 == 0)) {
     console.log('Buzz');
   }
   else if ((x % 3 == 0) && (x % 5 == 0)) {
